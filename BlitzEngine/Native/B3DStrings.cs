7cd4f3aab78e416224f13f08285517e29a4b9fad
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2019-06-14 16:52:46 -0400

    Added Blitz3D String Functions to C#

---
+++ BlitzEngine/Native/B3DStrings.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlitzEngine
+{
+	public static partial class Blitz3D
+	{
+		// ----
+		// Strings
+		// ----
+
+		[DllImport(B3DDllLink)]
+		private static extern void Left_internal(StringBuilder ret, int retLen, string str, int length);
+
+		public static string Left(string str, int length)
+		{
+			StringBuilder sb = new StringBuilder(4096);
+			Left_internal(sb, sb.Capacity, str, length);
+			return sb.ToString();
+		}
+
+		[DllImport(B3DDllLink)]
+		private static extern void Right_internal(StringBuilder ret, int retLen, string str, int length);
+
+		public static string Right(string str, int length)
+		{
+			StringBuilder sb = new StringBuilder(4096);
+			Right_internal(sb, sb.Capacity, str, length);
+			return sb.ToString();
+		}
+
+		[DllImport(B3DDllLink)]
+		private static extern void Mid_internal(StringBuilder ret, int retLen, string str, int offset, int length);
+
+		public static string Mid(string str, int offset, int length)
+		{
+			StringBuilder sb = new StringBuilder(4096);
+			Mid_internal(sb, sb.Capacity, str, offset, length);
+			return sb.ToString();
+		}
+
+		[DllImport(B3DDllLink)]
+		public static extern int Instr(string str1, string str2, int offset);
+	}
+}
--- BlitzSharp3D/mono/blitzlib.cpp
+++ BlitzSharp3D/mono/blitzlib.cpp
@@ -10,6 +10,7 @@
 #include "../bbruntime/bbsockets.h"
 #include "../bbruntime/bbaudio.h"
 #include "../bbruntime/bbbank.h"
+#include "../bbruntime/bbstring.h"
 
 #ifdef _WIN32
 #define PUBLIC_METHOD extern "C" __declspec(dllexport)
@@ -2899,4 +2900,40 @@ PUBLIC_METHOD void EntityClass_internal(char* ret, int retLen, Entity* entity)
 PUBLIC_METHOD void GetBuildType_internal(char* ret, int retLen)
 {
     strcpy_s(ret, retLen, toMonoStr(bbGetBuildType()));
+}
+
+// -------
+// Strings
+// -------
+
+PUBLIC_METHOD void Left_internal(char* ret, int retLen, const char* str, int length)
+{
+  BBStr* s = toBBStr(str);
+  strcpy_s(ret, retLen, toMonoStr(bbLeft(s, length)));
+  freeBBStr(s);
+}
+
+PUBLIC_METHOD void Right_internal(char* ret, int retLen, const char* str, int length)
+{
+  BBStr* s = toBBStr(str);
+  strcpy_s(ret, retLen, toMonoStr(bbRight(s, length)));
+  freeBBStr(s);
+}
+
+PUBLIC_METHOD void Mid_internal(char* ret, int retLen, const char* str, int offset, int length)
+{
+  BBStr* s = toBBStr(str);
+  strcpy_s(ret, retLen, toMonoStr(bbMid(s, offset, length)));
+  freeBBStr(s);
+}
+
+PUBLIC_METHOD int Instr(const char* str1, const char* str2, int offset)
+{
+  BBStr* s1 = toBBStr(str1);
+  BBStr* s2 = toBBStr(str2);
+  int ret = bbInstr(s1, s2, offset);
+  freeBBStr(s1);
+  freeBBStr(s2);
+
+  return ret;
 }
